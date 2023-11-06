1a90ef7e21f95272bdc1aad88d2e2c7802492cc4
Kamrul Hassan <kamrulthedev@gmail.com>
2023-11-06 20:28:34 +0600

    Home-React-commit

--- src/Compunents/ErroePage/ErroePage.jsx
+++ src/Compunents/ErroePage/ErroePage.jsx
@@ -1,12 +1,19 @@
+import { Link } from "react-router-dom";
 
 
 const ErroePage = () => {
     return (
-        <div className="text-5xl font-bold text-center mt-48 ">
-            <h1 className="mb-8">Opps !!</h1>
+        <div>
+           <div className="text-5xl font-bold text-center mt-48"> 
+           <h1 className="mb-8">Opps !!</h1>
             <h1 className="mb-8">404</h1>
             <p>Not Result</p>
+           </div>
+          <div className=" btn bg-violet-500 flex justify-center text-white font-bold rounded-xl mt-8">
+          <Link to={'/'}><button>GO TO HOME</button></Link>
+          </div>
         </div>
+        
     );
 };
 
--- src/Compunents/Home/Home.jsx
+++ src/Compunents/Home/Home.jsx
@@ -1,9 +1,11 @@
+import Banner from "../Banner/Banner";
 
 
 const Home = () => {
     return (
         <div>
-            <h2>This is Home page</h2>
+            <Banner></Banner>
+            
         </div>
     );
 };
