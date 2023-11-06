99309b9e259120d0d539f9eb9da00bb75db4ca6e
Kamrul Hassan <kamrulthedev@gmail.com>
2023-11-06 20:38:11 +0600

    category-commit

---
+++ src/Compunents/Featured/Featured.jsx
@@ -0,0 +1,14 @@
+
+
+const Featured = () => {
+    return (
+        <div>
+           <div className="text-center">
+           <h1 className="text-5xl font-bold">Featured Jobs</h1>
+            <p>Explore thousands of job opportunities with all the information you need. Its your future</p>
+           </div>
+        </div>
+    );
+};
+
+export default Featured;
