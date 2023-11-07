eeb0a0177669e85140e0d2cbf218f670bced4194
Kamrul Hassan <kamrulthedev@gmail.com>
2023-11-07 23:13:10 +0600

    Benner-section-commit

--- src/Compunents/Banner/Banner.jsx
+++ src/Compunents/Banner/Banner.jsx
@@ -3,7 +3,18 @@
 const Banner = () => {
     return (
         <div>
-           <h1 className="text-5xl font-bold text-center"> This is my Banner</h1> 
+           <div className="hero min-h-screen bg-zinc-50">
+  <div className="hero-content flex-col lg:flex-row-reverse">
+    <img src="https://i.ibb.co/JR4Y8Qs/user.png" className="max-w-sm rounded-lg " />
+    <div>
+      <h1 className="text-5xl font-bold text-zinc-950">One Step 
+      <br />Closer To Your
+      <br /> <span className="text-[#7E90FE]">Dream Job</span></h1>
+      <p className="py-6">Explore thousands of job opportunities with all the <br /> information you need. Its your future. Come find it. Manage all <br /> your job application from start to finish.</p>
+      <button className="btn btn-primary">Get Started</button>
+    </div>
+  </div>
+</div>
         </div>
     );
 };
